cd2fd970ce36815eba408141575d60d7acaccfb1
ziadazaherafeza <ziadafeza@gmail.com>
2022-06-11 12:04:34 +0200

    tp flutter

---
+++ tpflutter/android/app/src/main/kotlin/com/example/tpflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tpflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tpflutter/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:tpflutter/personne.dart';
+import 'package:tpflutter/update.dart';
+import 'package:tpflutter/deleteP.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // runApp(MaterialApp(
+  //   home: Personne(),
+  //   //home: HomePage(),
+  // ));
+  runApp(MaterialApp(
+    home: DeletePersonne(),
+    //home: HomePage(),
+  ));
+  //runApp(MaterialApp(
+    //home: EditPersonne(),
+    //home: HomePage(),
+  //));
+}
+
+
